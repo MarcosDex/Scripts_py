9e167384a2eb7f7ef036f21f9d70d57cfc09edf5
WalkerAway <54985051+MarcosDex@users.noreply.github.com>
2022-08-19 12:35:22 -0300

    correction on code

--- data de nascimento.py
+++ data de nascimento.py
@@ -1,4 +1,4 @@
 dia = input("What is your birth day?")
 mes = input("What is the month of your birth?")
 ano = input("What year were you born?")
-print("You were born in",mes,"on the",mes,"of the year",ano,)
+print("You were born in",mes,"on the",dia,"of the year",ano,)
