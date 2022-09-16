55716c9a708074a51830590bca9aae6931c981bb
WalkerAway <54985051+MarcosDex@users.noreply.github.com>
2022-09-16 08:59:25 -0300

    teste

--- sebastiao/exec19.py
+++ sebastiao/exec19.py
@@ -1,1 +1,7 @@
+coloque = int(input('Digite o numero que sera verificado no sistema:'))
+numero = [1,2,3,4,5,6,7,8,9]
 
+if coloque > 10:
+    print('Não existe esse numero')
+else:
+    print('O numero digitado foi', numero)
