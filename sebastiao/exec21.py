ef83b4f970f6c7760dd9de697e6aaddc7887120d
WalkerAway <54985051+MarcosDex@users.noreply.github.com>
2022-09-17 15:40:28 -0300

    op

---
+++ sebastiao/exec21.py
@@ -0,0 +1,5 @@
+cidade = input('Digite o nome de uma cidade')
+cidades = 'Panelas', 'Caruaru', 'Jurema', 'Lajedo', 'Garahuns', 'Cupira'.format(super)
+
+while cidade != cidades:
+    print('A cidade digitada foi:', cidade)
