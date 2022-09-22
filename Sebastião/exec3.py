0df9f0ab8f8945c647cca6eeeefb06bbd81f4503
WalkerAway <54985051+MarcosDex@users.noreply.github.com>
2022-09-21 23:10:30 -0300

    Delete Sebastião directory

--- Sebastião/exec3.py
+++
@@ -1,5 +0,0 @@
-#Faça um Programa que peça dois números e mostre como resultado a soma.
-number1 = int(input('Lets calc, insira um numero:'))
-number2 = int(input('Insira outro numero:'))
-
-print('A soma entre' ,number1, 'e' ,number2, 'é igual a:' ,number1+number2)
