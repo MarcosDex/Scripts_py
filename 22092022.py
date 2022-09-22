b647374e339d6baef33a37f8a5486e573bdbb4ac
WalkerAway <54985051+MarcosDex@users.noreply.github.com>
2022-09-22 20:22:39 -0300

    class created

---
+++ 22092022.py
@@ -0,0 +1,10 @@
+class carro:
+    def __init__(self, modelo):
+        self.modelo = modelo;
+        self.velocidade = 0
+    def acelerar(self):
+
+    def frear(self):
+    #input()
+    def acenderfarol(self):
+     #input()
