0c30ef5ab7526a94a92c2cc5494a2dbe86409170
WalkerAway <54985051+MarcosDex@users.noreply.github.com>
2022-09-21 23:01:09 -0300

    Add files via upload

---
+++ exec15.py
@@ -0,0 +1,35 @@
+#Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas
+#no mês. Calcule e mostre o total do seu salário no referido mês, sabendo-se que são descontados
+#11% para o Imposto de Renda, 8% para o INSS e 5% para o sindicato, faça um programa que nos dê:
+#a. salário bruto.
+#b. quanto pagou ao INSS.
+#c. quanto pagou ao sindicato.
+#d. o salário líquido.
+#e. calcule os descontos e o salário líquido, conforme a tabela abaixo
+
+#+ Salário Bruto : R$
+#- IR (11%) : R$
+#- INSS (8%) : R$
+#- Sindicato ( 5%) : R$
+#= Salário Liquido : R$
+#Obs.: Salário Bruto - Descontos = Salário Líquido.
+qnt_ganha = float(input('Quanto ganha por hora? '))
+horas_trabalhadas = int(input('Horas trabalhadas por mês: '))
+
+salario_bruto = qnt_ganha * horas_trabalhadas
+ir = salario_bruto * 0.11
+inss = salario_bruto * 0.08
+sindicato = salario_bruto * 0.05
+
+if qnt_ganha < 0:
+    print('A coisa ta dificil ein amigo!')
+
+elif horas_trabalhadas < 0:
+    print('Ta ganhando sem trabalhar? alo Elon musk')
+
+else:
+    print ('+ Salário Bruto : R$ %.2f' %salario_bruto)
+    print ('- IR: R$ %.2f' %ir)
+    print ('- INSS: R$ %.2f' %inss)
+    print ('- Sindicato: R$ %.2f' %sindicato)
+    print ('= Salário Liquido : R$ %.2f' %(salario_bruto - ir - inss - sindicato))
--- exec3.py
+++ exec3.py
@@ -1,19 +1,5 @@
-tempestadef = int(input("digite sua tempestade"))
-if tempestadef < 61 and tempestadef == 1:
-    print("tempestade fraca")
-elif tempestadef >= 62 and tempestadef <= 118:
-    print("tempestade  tropical")
-elif tempestadef >= 119 and tempestadef <= 152:
-    print("tempestade tempestuosa")
-elif tempestadef >= 153 and tempestadef <= 168:
-    print("tempestade formação 1")
-elif tempestadef >= 169 and tempestadef <= 200:
-    print("tempestade formação 2")
-elif tempestadef >= 201 and tempestadef <= 220:
-    print("tempestade formação 3")
-elif tempestadef >= 221 and tempestadef <= 240:
-    print("tempestade formação 4")
-elif tempestadef >= 241 and tempestadef >= 249:
-    print("voce esta no olho de uma tempestade FUJA")
-else:
-    print("voce não corre risco de tempestade na area")
+#Faça um Programa que peça dois números e mostre como resultado a soma.
+number1 = int(input('Lets calc, insira um numero:'))
+number2 = int(input('Insira outro numero:'))
+
+print('A soma entre' ,number1, 'e' ,number2, 'é igual a:' ,number1+number2)
