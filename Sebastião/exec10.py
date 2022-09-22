6fce9a96643faa9dc64f7b332ea2eaafd26b0f33
WalkerAway <54985051+MarcosDex@users.noreply.github.com>
2022-09-21 23:06:45 -0300

    Delete exec10.py

--- Sebastião/exec10.py
+++
@@ -1,6 +0,0 @@
-#Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
-celsius = input("Digite a temperatura em celsius: ")
-
-farenheit = ((float(celsius) + 32) / 5) * 9
-
-print("%.2f graus farenheit" % farenheit)
